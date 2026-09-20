03e52e74da72e4b6f437b9c2d3f5a766a08886b1
Bhupha Phanphaeng <bhupha_p@cmu.ac.th>
2026-09-20 11:07:24 +0700

    Mobile nav 3: Wire BottomNav into App.vue with matching content padding

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import AppHeader from '@/components/layout/AppHeader.vue'
 import AppFooter from '@/components/layout/AppFooter.vue'
+import BottomNav from '@/components/layout/BottomNav.vue'
 import AuthModal from '@/components/auth/AuthModal.vue'
 import OnboardingModal from '@/components/auth/OnboardingModal.vue'
 </script>
@@ -8,10 +9,11 @@ import OnboardingModal from '@/components/auth/OnboardingModal.vue'
 <template>
   <div class="flex h-screen flex-col overflow-hidden bg-surface">
     <AppHeader />
-    <div class="flex-1 overflow-y-auto">
+    <div class="flex-1 overflow-y-auto pb-16 sm:pb-0">
       <RouterView />
     </div>
     <AppFooter />
+    <BottomNav />
     <AuthModal />
     <OnboardingModal />
   </div>
